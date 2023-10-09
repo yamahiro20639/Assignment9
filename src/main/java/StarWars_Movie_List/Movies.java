1534b505f258056f3c8b3c050820e3edbc97eaad
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-10-09 20:50:06 +0900

    Moviesクラス作成

---
+++ src/main/java/StarWars_Movie_List/Movies.java
@@ -0,0 +1,39 @@
+package StarWars_Movie_List;
+
+public class Movies {
+    private int movie_id;
+    private String movieName;
+    private String releaseDate;
+
+    private int director_id;
+    private String director;
+
+    Movies(int movie_id, String movieName, String releaseDate, int director_id, String director) {
+        this.movie_id = movie_id;
+        this.movieName = movieName;
+        this.releaseDate = releaseDate;
+        this.director_id = director_id;
+        this.director = director;
+
+    }
+
+    public int getMovie_id() {
+        return movie_id;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public int getDirector_id() {
+        return director_id;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+}
