22d7d77c16d6f24efc9d91e4cb167d178e16bb20
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-10-09 21:13:04 +0900

    Servicの実装

--- src/main/java/StarWars_Movie_List/MoviesService.java
+++ src/main/java/StarWars_Movie_List/MoviesService.java
@@ -1,2 +1,22 @@
-package StarWars_Movie_List;public class MoviesService {
+package StarWars_Movie_List;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MoviesService {
+    public final MovieMapper movieMapper;
+
+    public MoviesService(MovieMapper movieMapper) {
+        this.movieMapper = movieMapper;
+    }
+
+    public List<Movies> getMovies() {
+
+        List<Movies> movies = movieMapper.findAll();
+        return movies;
+    }
+
 }
+
