bb6a9405f2ca205d28f39db018bdf0151465339b
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-10-09 18:17:02 +0900

    MySQLの構築

---
+++ src/main/java/StarWars_Movie_List/StarWarsMovieListApplication.java
@@ -0,0 +1,13 @@
+package StarWars_Movie_List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarWarsMovieListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarWarsMovieListApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/starwars/StarWars_Movie_List/StarWarsMovieListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starwars.StarWars_Movie_List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsMovieListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
