33dc9991b997d42a34e4c2eb9e15c438151d521a
yamahiro20639 <144509349+yamahiro20639@users.noreply.github.com>
2023-10-09 20:39:53 +0900

    マッパーの作成

--- src/main/java/StarWars_Movie_List/MovieMapper.java
+++ src/main/java/StarWars_Movie_List/MovieMapper.java
@@ -1,2 +1,13 @@
-package StarWars_Movie_List;public interface MovieMapper {
+package StarWars_Movie_List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+
+public interface MovieMapper {
+    @Select("SELECT * FROM movieName INNER JOIN director ON movie_id=director_id")
+    List<Movies> findAll();
 }
